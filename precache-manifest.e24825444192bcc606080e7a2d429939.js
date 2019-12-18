b677aad78e7ffcdbba998d7397d102fad4e91978
Ivan Deiko <ivandeiko97@gmail.com>
2019-12-18 20:17:36 +0300

    Updates

---
+++ precache-manifest.e24825444192bcc606080e7a2d429939.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0951e4a118589706536a99915baefb2",
+    "url": "/table/index.html"
+  },
+  {
+    "revision": "c7a23cdda77eb075bcc6",
+    "url": "/table/static/css/main.ab1d1be2.chunk.css"
+  },
+  {
+    "revision": "bcefbcabe30ff4d095ac",
+    "url": "/table/static/js/2.53e6b83b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/table/static/js/2.53e6b83b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7a23cdda77eb075bcc6",
+    "url": "/table/static/js/main.dd9fd4d2.chunk.js"
+  },
+  {
+    "revision": "52b232cdcd7757e1bf04",
+    "url": "/table/static/js/runtime-main.1885f316.js"
+  },
+  {
+    "revision": "ecef0faef83874e9f6bb9b5e9fa05a0e",
+    "url": "/table/static/media/ProximaNova-Regular.ecef0fae.woff2"
+  }
+]);
